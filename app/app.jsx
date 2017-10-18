5de19765b3402b2028709042e737242d2e4435c7
Julian Jorgensen <me@julianjorgensen.com>
2017-10-18 09:04:56 +0800

    Bugfix IE and safari blank screen issues

--- app/app.jsx
+++ app/app.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import shopifyAPI from 'shopifyAPI';
+require('es6-promise').polyfill();
 
 import fonts from './fonts/fonts.css';
 import 'applicationStyles';
