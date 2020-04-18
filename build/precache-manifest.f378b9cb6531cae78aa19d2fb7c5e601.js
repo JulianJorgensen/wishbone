0995d21187114a9177353afc5caba4745dcc0539
Julian Jorgensen <jj@julianjorgensen.com>
2020-04-18 15:48:18 +0200

    upgrade to create react app

---
+++ build/precache-manifest.f378b9cb6531cae78aa19d2fb7c5e601.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74ba08174aad4862fd737a61f9a8ac0f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5f83d3ff6f3d7a2c7bea",
+    "url": "/static/css/main.2f599869.chunk.css"
+  },
+  {
+    "revision": "b4ef2d506e3dcb27ddca",
+    "url": "/static/js/2.0a81757e.chunk.js"
+  },
+  {
+    "revision": "88cbcb55f867c7e879dbc5e47becbf3b",
+    "url": "/static/js/2.0a81757e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f83d3ff6f3d7a2c7bea",
+    "url": "/static/js/main.b043238c.chunk.js"
+  },
+  {
+    "revision": "96bd0bd5f6a2ec57d296",
+    "url": "/static/js/runtime-main.e50b7131.js"
+  },
+  {
+    "revision": "57b2b5fda60b290f0665dca256633b63",
+    "url": "/static/media/Nunito-Light.57b2b5fd.woff"
+  },
+  {
+    "revision": "7e4decf073b8bc3e1befb1a0c24724e6",
+    "url": "/static/media/Nunito-Light.7e4decf0.woff2"
+  },
+  {
+    "revision": "11af09755d1c58e061cb1e5846e32321",
+    "url": "/static/media/Nunito-Regular.11af0975.woff2"
+  },
+  {
+    "revision": "2b6295d79e9aa781a2718ddee56a4fc5",
+    "url": "/static/media/Nunito-Regular.2b6295d7.woff"
+  },
+  {
+    "revision": "216f277fe3dc58a9f0cc185bf51b44e0",
+    "url": "/static/media/Nunito-SemiBold.216f277f.woff"
+  },
+  {
+    "revision": "33ca9780ece8468abbd07bdce6a05f6b",
+    "url": "/static/media/Nunito-SemiBold.33ca9780.woff2"
+  }
+]);
